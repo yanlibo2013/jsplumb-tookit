b27f3284fe8db4a913b3cbf34e6831a58b3b47cd
unknown <yanlibo2010@163.com>
2019-09-01 20:53:13 +0800

    add src

---
+++ static/lib/utils/k.js
@@ -0,0 +1,34 @@
+eval(
+  (function(d, e, a, c, b, f) {
+    b = function(a) {
+      return (
+        (a < e ? "" : b(parseInt(a / e))) +
+        (35 < (a %= e) ? String.fromCharCode(a + 29) : a.toString(36))
+      );
+    };
+    if (!"".replace(/^/, String)) {
+      for (; a--; ) f[b(a)] = c[a] || b(a);
+      c = [
+        function(a) {
+          return f[a];
+        }
+      ];
+      b = function() {
+        return "\\w+";
+      };
+      a = 1;
+    }
+    for (; a--; )
+      c[a] && (d = d.replace(new RegExp("\\b" + b(a) + "\\b", "g"), c[a]));
+    return d;
+  })(
+    'w y(a){1S(a){9"F":9"U":9"2c":i"1i";t;9"V":9"29":i"1k";t;9"Y":9"N":9"Q":i"1v";t;9"H":9"E":9"I":i"1W";t;9"L":9"K":9"J":i"1Z";t;9"1B":9"27":9"2a":9"2b":9"2w":9"2h":9"2i":9"2l":9"2q":i"17";t;21:i"2r";t}}D 19={F:"F",U:"U",V:"V",H:"H",I:"I",1a:"1a",1b:"1b",1c:"1c",1d:"1d",1j:"1j",1l:"1l",L:"L",Q:"Q",K:"K",1o:"1o",1p:"1p",Y:"Y",N:"N",J:"J",E:"E",1t:"1t",1u:"1u",1x:"1x",1z:"1z",1A:"1A",1D:"1D",1H:"1H"};w 2m(a){r(a 2k 19){i"2j"}m{i T}}w A(a){1S(a){9"H":9"E":i"12";9"I":i"13";9"L":9"K":i"14";9"J":i"15";9"1B":i"2g"}}D P={18:2,R:"#S",2f:"2e",1e:"1f",1g:0};D v={18:3,R:"#28",1e:"1f",1g:0};D d={1m:["1n",{M:8}],1q:P,v:v,1r:{R:"#S",1s:"25",M:6,24:6},1w:G,1y:["23",{Z:[5,5],1C:10,1E:5,1F:G}],1G:T,j:1,1J:[["1K",{1L:10,1M:10,k:1}]]};D e={1m:["1n",{M:6}],1q:P,v:v,1r:{1s:"#S"},1w:T,1y:["2d",{Z:[5,5],1C:10,1E:5,1F:G}],1G:G,j:1,1J:[["1K",{1L:10,1M:10,k:1}]]};w X(a,b,c){a.1V();b.$1T(()=>{c.1P((W,1X)=>{1Y l=W.20,4=W.22;r(y(l)=="1i"){a.h(4,{g:"C",j:26},{f:4+"z"+"d",...d})}m r(y(l)=="1k"){a.h(4,{g:"x"},{f:4+"u"+"e",...e})}m r(y(l)=="1v"||y(l)=="17"){a.h(4,{g:"x",j:-1},{f:4+"u"+"e",...e});a.h(4,{g:"C",j:-1},{f:4+"z"+"d",...d})}m r(A(l)=="12"){a.h(4,{g:[1,0.3,0,0],j:-1,n:[["s",{k:[1.5,-0.5],q:"1h",p:"o"}]]},{f:4+"1h"+"d",...d});a.h(4,{g:[1,0.7,0,0],j:-1,n:[["s",{k:[1.5,1.3],q:"16",p:"o"}]]},{f:4+"16"+"d",...d});a.h(4,{g:"x"},{f:4+"u"+"e",...e})}m r(A(l)=="13"){a.h(4,{g:[1,0.3,0,0],j:-1,n:[["s",{k:[1.5,-0.5],q:"11",p:"o"}]]},{f:4+"11"+"d",...d});a.h(4,{g:[1,0.7,0,0],j:-1,n:[["s",{k:[1.5,1.3],q:"1R",p:"o"}]]},{f:4+"1R"+"d",...d});a.h(4,{g:"x"},{f:4+"u"+"e",...e})}m r(A(l)=="14"){a.h(4,{g:"C",j:-1},{f:4+"z"+"d",...d});a.h(4,{g:[0,0.3,0,0],n:[["s",{k:[-1,-0.5],q:"1Q",p:"o"}]]},{f:4+"1Q"+"e",...e});a.h(4,{g:[0,0.7,0,0],n:[["s",{k:[-1,1.5],q:"1O",p:"o"}]]},{f:4+"1O"+"e",...e})}m r(A(l)=="15"){a.h(4,{g:"C",j:-1},{f:4+"z"+"d",...d});a.h(4,{g:[0,0.3,0,0],n:[["s",{k:[-1,-0.5],q:"1N",p:"o"}]]},{f:4+"1N"+"e",...e});a.h(4,{g:[0,0.7,0,0],n:[["s",{k:[-1,1.5],q:"1I",p:"o"}]]},{f:4+"1I"+"e",...e})}m{a.h(4,{g:"C",j:-1},{f:4+"z"+"d",...d});a.h(4,{g:"x"},{f:4+"u"+"e",...e})}a.2n(4,{2o:"2p"})})})}w O(a,b,c){b.$1T(()=>{c.1P(B=>{a.2s({2t:[B.F+B.2u+"d",B.2v+B.u+"e"]})})})}1U.X=X;1U.O=O;',
+    62,
+    157,
+    "    dataIndex     case    origin destination uuid anchors addEndpoint return maxConnections location drawType else overlays endpointSourceLabel cssClass label if Label break input connectorHoverStyle function LeftMiddle nodeClass output specialNodeClass item RightMiddle var split source true decision validate minus productjoin join radius intersect ct connectorPaintStyle starjoin stroke 4e5568 false sqlsource sink data dd union stub  ok classD_D1 classD_D2 classD_E1 classD_E2 no classW_C strokeWidth nodeIconFont supplement sql transform filter outlineColor white outlineWidth yes classD_A sample classD_B lookup endpoint Dot aggregate top connectorStyle paintStyle fill Correlation Summary classD_C isSource gradientboogradientbostedtrees_predict connector MultilayerPerceptronPredict MultilayerPerceptronTrain dataflow gap kmeans_predict cornerRadius alwaysRespectStubs isTarget kmeans_train input2 connectorOverlays Arrow width length input1 right forEach left error switch nextTick window deleteEveryEndpoint classD_D index let classD_E type default id Flowchart lineWidth transparent 100 hawq ff4e4e show hive mapreduce streaming_source Straight round joinstyle classW_A spark event iconTrue in exclusive nodeIcon draggable containment parent parallel classO connect uuids sourceOutput target shell".split(
+      " "
+    ),
+    0,
+    {}
+  )
+);
